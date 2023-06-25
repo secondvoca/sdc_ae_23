67018ee0525284c011b487a35f58a2db12e51ecb
zisei <secondvoca@gmail.com>
2023-06-25 16:20:17 +0900

    ae demo success

--- myAE.py
+++ myAE.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+from tqdm import tqdm
 
 from collections import OrderedDict
 
@@ -14,7 +15,7 @@ class My_Encoder(nn.Module):
     def __init__(self, dim_encoder_output, activation):
         super().__init__()
         if activation == "tanh":
-            self.activation = F.tanh
+            self.activation = torch.tanh
         self.l0 = nn.Linear(784, 512)
         self.l1 = nn.Linear(512, 512)
         self.l2 = nn.Linear(512, dim_encoder_output)
@@ -30,7 +31,7 @@ class My_Decoder(nn.Module):
     def __init__(self, dim_decoder_input, activation):
         super().__init__()
         if activation == "tanh":
-            self.activation = F.tanh
+            self.activation = torch.tanh
         self.l0 = nn.Linear(dim_decoder_input, 512)
         self.l1 = nn.Linear(512, 512)
         self.l2 = nn.Linear(512, 784)
@@ -38,7 +39,7 @@ class My_Decoder(nn.Module):
     def forward(self, x):
         z = self.activation(self.l0(x))
         z = self.activation(self.l1(z))
-        z = F.sigmoid(self.l2(z))
+        z = torch.sigmoid(self.l2(z))
         return z
 
 
@@ -61,7 +62,9 @@ class SDC_AE:
         training_data.targets = training_data.targets[training_data.targets < less_than]
 
         # Create data loaders.
-        self.train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=shuffle)
+        self.train_dataloader = DataLoader(
+            training_data, batch_size=batch_size, shuffle=shuffle
+        )
 
     def prepare_model(self, dim_encoder_output, dim_decoder_input, activation="tanh"):
         if self.kind == "ae":
@@ -79,3 +82,52 @@ class SDC_AE:
                     ]
                 )
             )
+        self.optimizer = torch.optim.Adam(self.model.parameters())
+
+    def get_cuda_device_or_cpu(self):
+        if torch.cuda.is_available():
+            cuda_count = torch.cuda.device_count()
+
+            no = 0
+            mem_available = 0
+
+            for i in range(cuda_count):
+                tmp_available = torch.cuda.mem_get_info(i)[0]
+                if mem_available < tmp_available:
+                    no = i
+                    mem_available = tmp_available
+            return f"cuda:{no}"
+        return "cpu"
+
+    def run(self, model, dataloader, optimizer, device, calc_loss):
+        hist = torch.zeros(len(dataloader))
+
+        for batch, (x, y) in enumerate(dataloader):
+            x = x.view([-1, 28 * 28]).to(device)
+
+            # Compute prediction error
+            loss = calc_loss(model, x, F)
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            hist[batch] = loss.item()
+
+        return hist
+
+    def train(self, calc_loss, epochs=5):
+        device = self.get_cuda_device_or_cpu()
+        print(device)
+
+        self.model.to(device)
+        self.model.train()
+
+        hist = torch.zeros(0)
+
+        for _ in tqdm(range(epochs)):
+            tmp = self.run(self.model, self.train_dataloader, self.optimizer, device, calc_loss)
+            hist = torch.cat([hist, tmp])
+
+        return hist
